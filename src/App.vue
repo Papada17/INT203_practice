b523c2a0cb5f88b563fb1b6aa6bd43a773fb7e4a
Papada17 <papada.keaw@mail.kmutt.ac.th>
2022-04-02 21:46:19 +0700

    add practice

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import BaseButton from './components/BaseButton.vue'
+import { ref } from 'vue'
+const yText=ref('This is button');
+const yColor=ref('#00000')
+</script>
+ 
+<template>
+  <div>
+    <div>Your Text:<input type="text" v-model="yText"></div>
+    <BaseButton :yourText="yText"></BaseButton>
+    <div>
+      Your Color:{{ yColor }}<input type="color" v-model="yColor">
+    <BaseButton :yourText="yText" :yourColor="yColor"></BaseButton>
+
+    </div>
+  </div>
+</template>
+ 
+<style>
+
+</style>
---
+++ src/components/BaseButton.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from '@vue/reactivity';
+const prop = defineProps({
+    yourText:{
+        type:String,
+        require:true
+    },
+    yourColor:{
+        type:String,
+        require:true
+    }
+})
+// const buttonText = ref(prop.yourText) //ปุ่มไม่สามารถเปลี่ยนตามinputได้
+//const buttonText = computed(()=>prop.yourText) //computed เปลี่ยนไปตาม original ที่เปลี่ยน
+</script>
+ 
+<template>
+<!-- <div>
+    <button>{{ buttonText }} </button>
+</div> -->
+<div>
+    <button class="button-color">{{ yourText }}</button>
+</div>
+</template>
+ 
+<style>
+.button-color{
+    background-color: v-bind(yourColor);
+}
+</style>
